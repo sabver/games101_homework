b4595906b58a15759179c18b556d72f2a6b5c024
叶建锋 <1061022701@qq.com>
2023-02-21 21:34:13 +0900

    feat: Step 3: add more spheres

--- tinyraytracer/part1/tinyraytracer.cpp
+++ tinyraytracer/part1/tinyraytracer.cpp
@@ -5,12 +5,20 @@
 #include <vector>
 #include "geometry.h"
 
+struct Material
+{
+    Material(const Vec3f &color) : diffuse_color(color) {}
+    Material() : diffuse_color() {}
+    Vec3f diffuse_color;
+};
+
 struct Sphere
 {
     Vec3f center;
     float radius;
+    Material material;
 
-    Sphere(const Vec3f &c, const float &r) : center(c), radius(r) {}
+    Sphere(const Vec3f &c, const float &r, const Material &m) : center(c), radius(r), material(m) {}
 
     bool ray_intersect(const Vec3f &orig, const Vec3f &dir, float &t0) const
     {
@@ -37,18 +45,40 @@ struct Sphere
     }
 };
 
-Vec3f cast_ray(const Vec3f &orig, const Vec3f &dir, const Sphere &sphere)
+bool scene_intersect(const Vec3f &orig, const Vec3f &dir, const std::vector<Sphere> &spheres, Vec3f &hit, Vec3f &N, Material &material)
 {
-    float sphere_dist = std::numeric_limits<float>::max();
-    if (!sphere.ray_intersect(orig, dir, sphere_dist))
+    float spheres_dist = std::numeric_limits<float>::max();
+    for (size_t i = 0; i < spheres.size(); i++)
+    {
+        float dist_i;
+        // dist_i是相遇的时间t，t最小那个对应的圆就是需要展示的颜色
+        if (spheres[i].ray_intersect(orig, dir, dist_i) && dist_i < spheres_dist)
+        {
+            spheres_dist = dist_i;
+            // 交集点
+            hit = orig + dir * dist_i;
+            // 从圆心指向交集点
+            N = (hit - spheres[i].center).normalize();
+            material = spheres[i].material;
+        }
+    }
+    return spheres_dist < 1000;
+}
+
+Vec3f cast_ray(const Vec3f &orig, const Vec3f &dir, const std::vector<Sphere> &spheres)
+{
+    Vec3f point, N;
+    Material material;
+
+    if (!scene_intersect(orig, dir, spheres, point, N, material))
     {
         return Vec3f(0.2, 0.7, 0.8); // background color
     }
 
-    return Vec3f(0.4, 0.4, 0.3);
+    return material.diffuse_color;
 }
 
-void render(const Sphere &sphere)
+void render(const std::vector<Sphere> &spheres)
 {
     const int width = 1024;
     const int height = 768;
@@ -64,7 +94,7 @@ void render(const Sphere &sphere)
             float y = -(2 * (j + 0.5) / (float)height - 1) * tan(fov / 2.);
             // 这里必须要正则化
             Vec3f dir = Vec3f(x, y, -1).normalize();
-            framebuffer[i + j * width] = cast_ray(Vec3f(0, 0, 0), dir, sphere);
+            framebuffer[i + j * width] = cast_ray(Vec3f(0, 0, 0), dir, spheres);
         }
     }
 
@@ -85,7 +115,15 @@ void render(const Sphere &sphere)
 
 int main()
 {
-    Sphere sphere(Vec3f(-3, 0, -16), 2);
-    render(sphere);
+    Material ivory(Vec3f(0.4, 0.4, 0.3));
+    Material red_rubber(Vec3f(0.3, 0.1, 0.1));
+
+    std::vector<Sphere> spheres;
+    spheres.push_back(Sphere(Vec3f(-3, 0, -16), 2, ivory));
+    spheres.push_back(Sphere(Vec3f(-1.0, -1.5, -12), 2, red_rubber));
+    spheres.push_back(Sphere(Vec3f(1.5, -0.5, -18), 3, red_rubber));
+    spheres.push_back(Sphere(Vec3f(7, 5, -18), 4, ivory));
+
+    render(spheres);
     return 0;
 }
